bdb1b40560a294fc34eed4ed7ae43f3dfa437139
casluamorim <79804808+casluamorim@users.noreply.github.com>
2026-02-16 11:54:31 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+const decodedRedirect = redirect ? decodeURIComponent(redirect) : null;
+
+if (decodedRedirect) {
+  window.history.replaceState(null, "", decodedRedirect);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
